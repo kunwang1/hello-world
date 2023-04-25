679240aa8f7764be1f1fcbcc541341f601a2ff69
kunwang1 <46509063+kunwang1@users.noreply.github.com>
2023-04-25 10:26:06 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.ExecutorService;
+import javax.servlet.http.HttpServletRequest;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class HelloWorld {
+	
+	// Will remove after testing
+	public static void main(String[] args)
+	{
+		System.out.println("a story change");
+		
+		stringEqualityCheckNoncompliant("hello", "hello");
+		executeSqlStatementNoncompliant(null, null);
+		keyPairGeneratorNoncompliant();
+	}
+	
+	public static void stringEqualityCheckNoncompliant(String string1, String string2) {
+    	// Noncompliant: the == operator doesn't compare the contents of the strings.
+   		if(string1 == string2) {
+        		System.out.println("The strings are equal.");
+    		}
+	}
+	
+	static void shutdownNonCompliant(ExecutorService executorService) throws InterruptedException {
+	    if (executorService != null) {
+		// Noncompliant: shutdownNow is called which suddenly shuts down executorService.
+		executorService.shutdownNow();
+	    }
+	}
+	
+	public static void executeSqlStatementNoncompliant(HttpServletRequest request, java.sql.Connection connection) {
+	    final String favoriteColor = request.getParameter("favoriteColor");
+	    try {
+		String sql = "SELECT * FROM people WHERE favorite_color='" + favoriteColor + "'";
+		java.sql.Statement statement = connection.createStatement();
+		// Noncompliant: user-given input is not sanitized before use.
+		statement.execute(sql);
+	    } catch (java.sql.SQLException e) {
+		throw new RuntimeException(e);
+	    }
+	}
+	
+	public static void keyPairGeneratorNoncompliant() throws NoSuchAlgorithmException {
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		// Noncompliant: keysize too small for this algorithm.
+		keyPairGenerator.initialize(128);
+		keyPairGenerator.genKeyPair();
+	}
+
+}
